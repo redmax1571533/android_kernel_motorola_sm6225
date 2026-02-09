bbda0d4e9babc94a9704988abcf452c983e31150
redmax1571533 <marcoferreira0098@gmail.com>
2026-02-09 03:44:08 -0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup
 
 	# Download clang if not present
 	if [[ ! -d clang ]]; then mkdir -p clang
-		wget https://github.com/ZyCromerZ/Clang/releases/download/23.0.0git-20260130-release/Clang-23.0.0git-20260130.tar.gz -O clang.tar.gz
+		wget https://github.com/Impqxr/aosp_clang_ci/releases/download/13289611/clang-13289611-linux-x86.tar.xz -O clang.tar.gz
 		tar -xf clang.tar.gz -C clang && if [ -d clang/clang-* ]; then mv clang/clang-*/* clang; fi && rm -rf clang.tar.gz
 	fi
 
@@ -15,16 +15,7 @@ curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup
 	make O=out ARCH=arm64 guamp_defconfig
 
 	# Build the kernel with clang and log output
-	make -j$(nproc --all) O=out ARCH=arm64 \
-    CC=clang \
-    LLVM=1 \
-    LLVM_IAS=1 \
-    LD=ld.lld \
-    AR=llvm-ar \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE=aarch64-linux-android- \
-    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-    KCFLAGS="-w"
+	make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 2>&1
 
 cd out/arch/arm64/boot
 
